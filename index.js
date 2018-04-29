0caa2a3e09ff65bc9e4736e7c638dd50acbea297
ProPillow <wilsonshum96@hotmail.com>
2018-04-28 17:05:07 -0700

    update

--- index.js
+++ index.js
@@ -6,35 +6,30 @@ var app = express();
 var server = require('http').Server(app);
 
 const SERVER_PORT = 8000;
-const UUID  = "7862031e-e253-4bc2-81d5-7bb42276cd5d";
-const IP = '13.78.130.82:51010';
-
-bluzelle.connect('ws://'+ IP, UUID);
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/cilent/index.html');
+    res.sendFile(__dirname + '/crypto-js/index.html');
 });
 
-app.use('/cilent', express.static(__dirname + '/cilent'));
+app.use('/crypto-js', express.static(__dirname + '/crypto-js'));
 
 server.listen(process.env.PORT || SERVER_PORT);
 console.log('Server Started! localhost: ' + SERVER_PORT);
 
 
-// bluzelle.create('mykey', { a: 13 }).then(() => { 
-//     console.log("data created!")
-// }, error => {console.log(error)});
 
-// bluzelle.has('mykey').then(hasMyKey => {
-//     console.log(hasMyKey);
-// }, error => { console.log(error) });
+const IP = '13.78.130.82:51010';
+const UUID  = "7862031e-e253-4bc2-81d5-7bb42276cd5d";
+
+bluzelle.connect('ws://13.78.130.82:51010', UUID);
+console.log('Bluzelle Connected!');
+
 
 var fileContentHash = "13";
 
 bluzelle.read('mykey').then(obj => {
-    console.log(obj); 
-    // const fileName = 'a';
-    // console.log(obj[fileName]);
+    console.log(obj);
+
     for(key in obj) {
         var value = obj[key];
         //console.log (key, value);
@@ -49,3 +44,5 @@ bluzelle.read('mykey').then(obj => {
 .catch(function (error) {
      console.log("Promise Rejected: " + error);
 });
+
+
