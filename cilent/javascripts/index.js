12a5c81f737d862b8418832512f3e54a85391789
nidhogg5 <nidhogg5@yahoo.com.tw>
2018-04-29 15:46:52 -0700

    vue

--- cilent/javascripts/index.js
+++ cilent/javascripts/index.js
@@ -1,5 +1,53 @@
+const ICON = {
+    question: 'question circle outline',
+    check: 'check',
+    loading: 'notched circle loading',
+    times: 'times',
+}
+
+const COLOR = {
+    info: 'info',
+    warning: 'warning',
+    positive: 'positive',
+    negative: 'negative',
+}
+
 $(document).ready(() => {
     $('.menu .item').tab()
+})
+
+let console_register = new Vue({
+    el: '#console_register',
+    data: {
+        color: '',
+        icon: ICON.question,
+        message: 'please select file',
+    }
+})
+
+let console_query = new Vue({
+    el: '#console_query',
+    data: {
+        color: '',
+        icon: ICON.question,
+        message: 'please select file',
+    }
+})
+
+let table_register = new Vue({
+    el: '#table_register',
+    data: {
+        fileName: '',
+        checksum: '',
+    }
+})
+
+let table_query = new Vue({
+    el: '#table_query',
+    data: {
+        fileName: '',
+        checksum: '',
+    }
 })
 
 function dragoverHandler(evt) {
@@ -12,26 +60,24 @@ function dropHandler_register(evt) { //evt 為 DragEvent 物件
     let file = evt.dataTransfer.files[0]
     let reader = new FileReader()
 
-    $('#register-console i').removeClass().addClass('notched circle loading icon')
+    console_register.color = ''
+    console_register.icon = ICON.loading
+    console_register.message = 'loading'
 
     // If we use onloadend, we need to check the readyState.
     reader.onloadend = function (evt) {
         if (evt.target.readyState == FileReader.DONE) { // DONE == 2
-            $('#register-console').removeClass().addClass('ui positive icon message')
-            $('#register-console i').removeClass().addClass('check icon')
-            $('#register-console div').text('ready to register')
+            console_register.color = COLOR.info
+            console_register.icon = ICON.check
+            console_register.message = 'ready to register'
 
             let wordArray = CryptoJS.lib.WordArray.create(evt.target.result)
             let hash = CryptoJS.SHA256(wordArray)
 
             console.log(file)
 
-            $('#file_name-register').text(file.name)
-            $('#checksum-register').text(hash.toString())
-        }
-        else {
-            $('#register-console').addClass('negative')
-            $('#register-console div').text('read error')
+            table_register.fileName = file.name
+            table_register.checksum = hash.toString()
         }
     }
     reader.readAsArrayBuffer(file)
@@ -43,26 +89,22 @@ function dropHandler_query(evt) { //evt 為 DragEvent 物件
     let file = evt.dataTransfer.files[0]
     let reader = new FileReader()
 
-    $('#query-console i').removeClass().addClass('notched circle loading icon')
+    console_query.color = ''
+    console_query.icon = ICON.loading
+    console_query.message = 'loading'
 
     // If we use onloadend, we need to check the readyState.
     reader.onloadend = function (evt) {
         if (evt.target.readyState == FileReader.DONE) { // DONE == 2
-            $('#query-console').removeClass().addClass('ui positive icon message')
-            $('#query-console i').removeClass().addClass('check icon')
-            $('#query-console div').text('ready to verify')
+            console_query.color = COLOR.info
+            console_query.icon = ICON.check
+            console_query.message = 'ready to verify'
 
             let wordArray = CryptoJS.lib.WordArray.create(evt.target.result)
             let hash = CryptoJS.SHA256(wordArray)
 
-            console.log(file)
-
-            $('#file_name-query').text(file.name)
-            $('#checksum-query').text(hash.toString())
-        }
-        else {
-            $('#query-console').addClass('negative')
-            $('#query-console div').text('read error')
+            table_query.fileName = file.name
+            table_query.checksum = hash.toString()
         }
     }
 
