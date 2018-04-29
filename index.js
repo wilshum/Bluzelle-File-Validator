572f88bc9bb6986bde6503a6afbe1ebef8ef5f21
ProPillow <wilsonshum96@hotmail.com>
2018-04-28 17:41:25 -0700

    new IP

--- index.js
+++ index.js
@@ -16,33 +16,42 @@ app.use('/crypto-js', express.static(__dirname + '/crypto-js'));
 server.listen(process.env.PORT || SERVER_PORT);
 console.log('Server Started! localhost: ' + SERVER_PORT);
 
-
-
-const IP = '13.78.130.82:51010';
+const IP = '13.78.131.140:51010';
 const UUID  = "7862031e-e253-4bc2-81d5-7bb42276cd5d";
 
-bluzelle.connect('ws://13.78.130.82:51010', UUID);
+bluzelle.connect('ws://' + IP, UUID);
 console.log('Bluzelle Connected!');
 
 
 var fileContentHash = "13";
 
-bluzelle.read('mykey').then(obj => {
-    console.log(obj);
 
-    for(key in obj) {
-        var value = obj[key];
-        //console.log (key, value);
-        if (value == fileContentHash){
-            console.log("file: " + key + " is Valid");
-        }
-        else{
-            console.log("file: " + key + " is Invalid");
-        }
-    }
-}, error => { console.log(error) })
-.catch(function (error) {
-     console.log("Promise Rejected: " + error);
+bluzelle.has('mykey').then(fileName => { 
+    console.log("has file"); }, error => { console.log(error); 
 });
+// bluzelle.create('mykey', { d: 13 }).then(() => {
+//  console.log("first key") }, error => { console.log(error) 
+// });
+
+bluzelle.update('mykey', { b: 15 }).then(() => { console.log('updated');}, error => { console.log(error); });
+
+
+// bluzelle.read('mykey').then(obj => {
+//     console.log(obj);
+
+//     for(key in obj) {
+//         var value = obj[key];
+//         //console.log (key, value);
+//         if (value == fileContentHash){
+//             console.log("file: " + key + " is Valid");
+//         }
+//         else{
+//             console.log("file: " + key + " is Invalid");
+//         }
+//     }
+// }, error => { console.log(error) })
+// .catch(function (error) {
+//      console.log("Promise Rejected: " + error);
+// });
 
 
